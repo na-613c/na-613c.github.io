5a9ec20bc055cf7779e56f99489568f4fa976755
Shavlovsii_Ivan <na613c613c@gmail.com>
2020-12-16 17:44:40 +0300

    [ADD] Footer text and back-top button style

--- src/components/MyFooter/MyFooter.tsx
+++ src/components/MyFooter/MyFooter.tsx
@@ -1,10 +1,16 @@
-import { Footer } from 'antd/lib/layout/layout';
+import {Footer} from 'antd/lib/layout/layout';
 import React from 'react';
+
+const Style = {
+    background: 'rgba(239, 219, 255,0.2)',
+    boxShadow: '0 0 15px rgba(0,0,0,0.5)',
+    marginBottom: 10,
+};
 
 const MyFooter = () => {
 
     return (
-        <Footer> FOOTER </Footer>
+        <Footer style={Style}>© 2020 Shaulouski Ivan</Footer>
     );
 };
 
